335c6c373daac9577bf425b97f45dfa798b0bb0a
Hankerr <15109212549@163.com>
2022-01-24 23:39:41 +0800

    ToDo模块

--- MyToDo/ViewModels/MainViewModel.cs
+++ MyToDo/ViewModels/MainViewModel.cs
@@ -65,7 +65,7 @@ namespace MyToDo.ViewModels
         void CreateMenuBar()
         {
             MenuBars.Add(new MenuBar() { Icon = "Home", Title = "首页", NameSpace = "IndexView" });
-            MenuBars.Add(new MenuBar() { Icon = "NotebookOutline", Title = "待办事项", NameSpace = "ToDOView" });
+            MenuBars.Add(new MenuBar() { Icon = "NotebookOutline", Title = "待办事项", NameSpace = "ToDoView" });
             MenuBars.Add(new MenuBar() { Icon = "NotebookPlus", Title = "备忘录", NameSpace = "MemoView" });
             MenuBars.Add(new MenuBar() { Icon = "Cog", Title = "设置", NameSpace = "SettingsView" });
         }
--- MyToDo/ViewModels/ToDoViewModel.cs
+++ MyToDo/ViewModels/ToDoViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using MyToDo.Common;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,7 @@ namespace MyToDo.ViewModels
         {
             ToDoDtos = new ObservableCollection<ToDoDto>();
             CreatToDoList();
+            AddCommand = new DelegateCommand(Add);
         }
 
         private ObservableCollection<ToDoDto> toDoDtos;
@@ -25,11 +27,28 @@ namespace MyToDo.ViewModels
             set { toDoDtos = value; RaisePropertyChanged(); }
         }
 
+        private bool isRightDrawerOpen;
+        /// <summary>
+        /// 右侧编辑窗口是否展开
+        /// </summary>
+        public bool IsRightDrawerOpen
+        {
+            get { return isRightDrawerOpen; }
+            set { isRightDrawerOpen = value; RaisePropertyChanged(); }
+        }
+
+        private void Add()
+        {
+            IsRightDrawerOpen = true;
+        }
+
+        public DelegateCommand AddCommand { get; private set; }
+
         private void CreatToDoList()
         {
             for (int i = 0; i < 10; i++)
             {
-                toDoDtos.Add(new ToDoDto() { Title = "Todo" + i, Content = "测试数据..." });
+                toDoDtos.Add(new ToDoDto() { Title = "标题" + i, Content = "测试数据..." });
             }
         }
     }
