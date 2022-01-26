d5097e252636960ace54e20d06fce9c11378ee12
Hankerr <15109212549@163.com>
2022-01-26 21:45:07 +0800

    记事本功能提交

--- MyToDo/ViewModels/MemoViewModel.cs
+++ MyToDo/ViewModels/MemoViewModel.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using MyToDo.Common;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyToDo.ViewModels
 {
-    class MemoViewModel
+    public class MemoViewModel : BindableBase
     {
+        public MemoViewModel()
+        {
+            MemoDtos = new ObservableCollection<MemoDto>();
+            CreatMemosList();
+            AddCommand = new DelegateCommand(Add);
+        }
+
+        private ObservableCollection<MemoDto> memoDtos;
+
+        public ObservableCollection<MemoDto> MemoDtos
+        {
+            get { return memoDtos; }
+            set { memoDtos = value; RaisePropertyChanged(); }
+        }
+
+        private bool isRightDrawerOpen;
+        /// <summary>
+        /// 右侧编辑窗口是否展开
+        /// </summary>
+        public bool IsRightDrawerOpen
+        {
+            get { return isRightDrawerOpen; }
+            set { isRightDrawerOpen = value; RaisePropertyChanged(); }
+        }
+
+        private void Add()
+        {
+            IsRightDrawerOpen = true;
+        }
+
+        public DelegateCommand AddCommand { get; private set; }
+
+        private void CreatMemosList()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                MemoDtos.Add(new MemoDto() { Title = "标题" + i, Content = "测试数据..." });
+            }
+        }
     }
 }
